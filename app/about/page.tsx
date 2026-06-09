64a614419fbb3c6eb1cec0098f6c58af3ad46c50
takutolow09@gmail.com <takutolow09@gmail.com>
2026-06-09 20:45:45 +0900

    Fix build errors for deployment

--- app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-const AboutPage = () => {
-    return (
-        <main className="p-8">
-            <h1 className="text-2xl font-bold">About</h1>
-        </main>
-    );
-};
-
-export default AboutPage;
